ffb9dab0e50c06aed1caf1760579ab07e0f17566
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2022-08-31 15:04:39 +0430

    change carousel component for banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,13 +2,28 @@ import React from 'react';
 import banner_1 from "../../assets/images/banner-1.jpg";
 import banner_2 from "../../assets/images/banner-2.jpg";
 import classNames from "classnames";
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
+import Carousel from 'react-multi-carousel';
 import styles from "./Banner.module.scss"
 
 
 export default function Banner(props)
 {
+    
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1279 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 1279, min: 767 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 767, min: 0 },
+      items: 1
+    }
+  };
     var items = [
         {
             title: "Welcome To Fashion",
@@ -23,27 +38,33 @@ export default function Banner(props)
             buttonText: "SHOP NOW",
         }
     ]
-    return (
-        <Carousel>
-            {
-                items.map( (item, i) => <Item key={i} item={item}/> )
-            }
-        </Carousel>
+    return (<>
+        <div className='overflow-hidden w-[100%]'>
+                <Carousel responsive={responsive} autoPlay={true} infinite={true} arrows={true}>
+                {
+                    items.map((item,index)=>{
+                        return <Item item={item}/>
+                    })
+                }
+                </Carousel>
+        </div>
+        </>
     )
 }
 
 function Item(props)
 {
+    
     return (
-        <Paper className={styles.paper} style={{backgroundImage: `url(${props.item.class})`}}>
+        <div className={styles.paper} style={{backgroundImage: `url(${props.item.class})`}}>
             <div className={styles.textPaper}>
                 <p>{props.item.title}</p>
                 <h1>{props.item.name}</h1>
 
-                <Button className={classNames("CheckButton","btn","btn-solid",styles.shopButton)}>
+                <button className={classNames("CheckButton","btn","btn-solid",styles.shopButton)}>
                     {props.item.buttonText}
-                </Button>
+                </button>
             </div>
-        </Paper>
+        </div>
     )
 }
